3082fc188b322ddcdfa55452c795a170977fd18e
tomyr95 <tomyr95@me.com>
2018-09-10 12:09:41 -0400

    for ProgrammingAssignment2
    The pair of functions 'makeCacheMatrix' and 'cacheSolve' work in conjuction to both produce the inverse solution of an invertible matrix and if the value has already been calculated provide that cached value w/o calculation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of functions 'makeCacheMatrix' and 'cacheSolve' work in conjuction
+## to both produce the inverse solution of an invertible matrix and if the value
+## has already been calculated provide that cached value w/o calculation.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates special "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){  #initiate function argument x as matrix
+    x <- as.data.frame(x)                   #change to atomic data as dataframe
+    s <- NULL                               #initiate s (solution)
+    set <- function(y) {                    #assignments in parent environment
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x                     #define getter for x
+    setsoln <- function(solve) s <<- solve  #define getter for solve
+    getsoln <- function() s                 #define setter for solution s
+    list(set = set, get = get,              #define list for $ operator usage
+         setsoln = setsoln,
+         getsoln = getsoln)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes or returnes cached cvalue of the inverse of the special "matrix" 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {            #inititate function, additional args
+    s <- x$getsoln()                        #attempt to retreave existing soln
+    if(!is.null(s)) {                       #used cached data is soln exists
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()                         #get input object, calculate soln
+    s <- solve(data, ...)
+    x$setsoln(s)
+    sm <- as.matrix(s)                      #provide soln in matrix format
+    rownames(sm) <- NULL                    #remove indexes for look
+    sm                                      #return soln matrix
 }
